a7361ff62dc27c9a4b85f115b05f1e9c73d5a28b
thatanjan <anjan.cules@gmail.com>
2020-12-30 18:14:28 +0600

    paper background color is changed

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -3,6 +3,9 @@ import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles'
 const darkTheme = createMuiTheme({
 	palette: {
 		type: 'dark',
+		background: {
+			paper: '#1a1a1a',
+		},
 	},
 })
 
