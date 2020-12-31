7379fad44e156c164760083e73f172c7a4c178ae
thatanjan <anjan.cules@gmail.com>
2020-12-31 08:48:27 +0600

    a demo navigation is added

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -4,6 +4,8 @@ import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core/styles'
 
+import Navigation from 'components/Navigation/Navigation'
+
 interface Props {
 	Content?: ReactNode
 }
@@ -38,6 +40,7 @@ const PageLayout = ({ Content }: Props) => {
 						{Content}
 					</Grid>
 				</Grid>
+				<Navigation />
 			</Paper>
 		</>
 	)
