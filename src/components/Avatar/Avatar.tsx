0416967cf9b77646f1c46065fad0fd2094a9bbe3
thatanjan <anjan.cules@gmail.com>
2021-01-01 23:53:32 +0600

    some styles added to the image component

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
 import Image from 'next/image'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+
+const useStyles = makeStyles({
+	avatarStyle: {
+		position: 'relative',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		flexShrink: 0,
+		borderRadius: '50%',
+		overflow: 'hidden',
+		objectFit: 'cover',
+	},
+})
 
 const Avatar = () => {
-	return <Image src='/ww.jpg' height={0} width={0} layout='responsive' />
+	const { avatarStyle } = useStyles()
+	return (
+		<Image
+			className={avatarStyle}
+			src='/ww.jpg'
+			height={0}
+			width={0}
+			layout='responsive'
+		/>
+	)
 }
 
 export default Avatar
