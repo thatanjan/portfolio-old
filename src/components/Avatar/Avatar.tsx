6ee034d143a8d33c6df6dc049a65c065750725f4
Anjan18 <anjanbiber@gmail.com>
2021-01-01 18:19:41 +0600

    A avatar comonent for about page

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Avatar = () => {
+	return <Image src='/ww.jpg' height={0} width={0} layout='responsive' />
+}
+
+export default Avatar
