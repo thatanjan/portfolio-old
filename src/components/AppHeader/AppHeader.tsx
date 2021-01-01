2b09cb56a42688c11170b227b3895f8578de0202
thatanjan <anjan.cules@gmail.com>
2021-01-01 11:18:08 +0600

    Menu icon will be removed from appHeader if the screen size is larger than MD

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -6,6 +6,8 @@ import Typography from '@material-ui/core/Typography'
 
 import MenuIcon from 'components/Menu/MenuIcon'
 
+import useLargerThanMD from 'hooks/useLargerThanMD'
+
 const useStyles = makeStyles((theme: Theme) => ({
 	root: {
 		width: '100vw',
@@ -22,6 +24,8 @@ const useStyles = makeStyles((theme: Theme) => ({
 export default function ButtonAppBar() {
 	const { root, titleStyle, toolbarStyle } = useStyles()
 
+	const largerThanMD = useLargerThanMD()
+
 	return (
 		<AppBar className={root} position='static' color='transparent'>
 			<Toolbar className={toolbarStyle}>
@@ -29,7 +33,7 @@ export default function ButtonAppBar() {
 					Anjan
 				</Typography>
 
-				<MenuIcon />
+				{!largerThanMD && <MenuIcon />}
 			</Toolbar>
 		</AppBar>
 	)
