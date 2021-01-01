8c7c3fa3e43fe34e42352dee3a9495b7fcf6117b
thatanjan <anjan.cules@gmail.com>
2021-01-01 12:31:02 +0600

    A top navigation which has a menu icon. when it will be clicked a drawer will pop up.

---
+++ src/components/Navigation/TopNavigation.tsx
@@ -0,0 +1,42 @@
+import React, { useState, MouseEvent, KeyboardEvent } from 'react'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
+
+import FullWidthTabs from 'components/Tabs/FullWidthTabs'
+
+import MenuIcon from 'components/Menu/MenuIcon'
+
+const TopNavigation = () => {
+	const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+
+	const toggleDrawer = (open: boolean) => (
+		event: KeyboardEvent | MouseEvent
+	) => {
+		if (
+			event &&
+			event.type === 'keydown' &&
+			((event as KeyboardEvent).key === 'Tab' ||
+				(event as KeyboardEvent).key === 'Shift')
+		) {
+			return
+		}
+
+		setIsDrawerOpen(open)
+	}
+
+	return (
+		<>
+			<MenuIcon toggle={toggleDrawer(true)} />
+
+			<SwipeableDrawer
+				anchor='left'
+				open={isDrawerOpen}
+				onClose={toggleDrawer(false)}
+				onOpen={toggleDrawer(true)}
+			>
+				<FullWidthTabs />
+			</SwipeableDrawer>
+		</>
+	)
+}
+
+export default TopNavigation
