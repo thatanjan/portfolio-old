91ff020a39a4a9dd4013aa4ecf816626b8e5f320
thatanjan <anjan.cules@gmail.com>
2021-01-02 19:00:23 +0600

    '/' route will render nothing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,3 @@
-import Head from 'next/head'
-import Typography from '@material-ui/core/Typography'
-
 export default function Home() {
-	return (
-		<div className='app'>
-			<Head>
-				<title>Create Next App</title>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-
-			<Typography>Hello World</Typography>
-		</div>
-	)
+	return null
 }
