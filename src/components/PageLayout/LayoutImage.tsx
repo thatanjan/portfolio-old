050e83a078108567394f9e6b8c7dd9626a775ff9
thatanjan <anjan.cules@gmail.com>
2021-01-02 17:44:55 +0600

    image component for layout image with style

---
+++ src/components/PageLayout/LayoutImage.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+
+import StyledImage from 'components/Images/StyledImage'
+
+const useStyles = makeStyles({
+	imageStyle: {},
+	containerStyle: {
+		width: '85%',
+		height: '90%',
+		display: 'grid',
+		alignItems: 'end',
+		'& > div': {
+			height: 'inherit',
+		},
+	},
+})
+
+const LayoutImage = () => {
+	const { imageStyle, containerStyle } = useStyles()
+
+	return (
+		<>
+			<div className={containerStyle}>
+				<StyledImage styleClass={imageStyle} />
+			</div>
+		</>
+	)
+}
+
+export default LayoutImage
