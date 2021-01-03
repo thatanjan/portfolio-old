7d35e29bc4c6a3191a4b9cdcfd6c74f41b931a21
thatanjan <anjan.cules@gmail.com>
2021-01-03 11:38:59 +0600

    index.tsx file is removed and about.tsx is renamed to index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-	return null
+import Avatar from 'components/Avatar/Avatar'
+import useLargerThanMD from 'hooks/useLargerThanMD'
+import AboutMe from 'components/AboutMe/AboutMe'
+
+const About = () => {
+	const largerThanMD = useLargerThanMD()
+
+	return (
+		<>
+			{!largerThanMD && <Avatar />} <AboutMe />{' '}
+		</>
+	)
 }
+
+export default About
