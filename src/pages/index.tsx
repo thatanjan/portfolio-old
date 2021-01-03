74ca7de8460c92ea1b4fc5f3a5967479b926e1d9
thatanjan <anjan.cules@gmail.com>
2021-01-03 11:55:31 +0600

    head added to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,20 @@
+import Head from 'next/head'
 import Avatar from 'components/Avatar/Avatar'
 import useLargerThanMD from 'hooks/useLargerThanMD'
 import AboutMe from 'components/AboutMe/AboutMe'
 
-const About = () => {
+const Home = () => {
 	const largerThanMD = useLargerThanMD()
 
 	return (
 		<>
+			<Head>
+				<title>Anjan</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 			{!largerThanMD && <Avatar />} <AboutMe />{' '}
 		</>
 	)
 }
 
-export default About
+export default Home
