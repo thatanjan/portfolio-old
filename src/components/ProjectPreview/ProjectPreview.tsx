4eb6de7fdc488e4cb4bc89198f0c815c307e4949
thatanjan <anjan.cules@gmail.com>
2021-01-22 00:34:08 +0600

    box component is wrapped by the image. some styles are also added

--- src/components/ProjectPreview/ProjectPreview.tsx
+++ src/components/ProjectPreview/ProjectPreview.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Box from '@material-ui/core/Box'
+
+import StyledImage from 'components/Images/StyledImage'
 
 interface Props {}
 
+const useStyles = makeStyles({
+	imageStyle: {},
+	boxStyle: { margin: '0 auto', width: '80%' },
+})
+
 const ProjectPreview = (props: Props) => {
-	return <div>This is Project preview</div>
+	const { imageStyle, boxStyle } = useStyles()
+
+	return (
+		<>
+			<Box className={boxStyle}>
+				<StyledImage styleClass={imageStyle} />
+			</Box>
+		</>
+	)
 }
 
 export default ProjectPreview
