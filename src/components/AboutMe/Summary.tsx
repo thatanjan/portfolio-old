c64ab2dcc8fed0c3a0a695cffd0f96a5bff6813b
Anjan18 <anjanbiber@gmail.com>
2021-01-21 23:19:52 +0600

    a short summary about myself

---
+++ src/components/AboutMe/Summary.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+const Summary = () => {
+	return (
+		<>
+			<Typography variant='h4'>
+				passionate about technolgies and contribute to this world for greater
+				change.
+			</Typography>
+		</>
+	)
+}
+
+export default Summary
