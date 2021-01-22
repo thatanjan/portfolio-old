3564ad7f9b520306a6dc61bf884fb799522d2c76
thatanjan <anjan.cules@gmail.com>
2021-01-22 11:24:43 +0600

    two button added to visit and learn more

--- src/components/ProjectPreview/ProjectPreview.tsx
+++ src/components/ProjectPreview/ProjectPreview.tsx
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
 import StyledImage from 'components/Images/StyledImage'
 
@@ -11,6 +12,9 @@ interface Props {
 	subtitle: string
 }
 
+const commonContaienrStyle = {
+	padding: '0 10%',
+}
 const useStyles = makeStyles({
 	imageStyle: {},
 	boxStyle: {
@@ -24,15 +28,28 @@ const useStyles = makeStyles({
 	},
 
 	titleGridContainer: {
-		padding: '0 10%',
+		...commonContaienrStyle,
 		'& > .MuiGrid-item': {
 			flexBasis: '100%',
+		},
+	},
+	buttonContainer: {
+		...commonContaienrStyle,
+		paddingTop: '2%',
+		paddingBottom: '2%',
+		'&  .MuiGrid-item': {
+			flexBasis: '50%',
 		},
 	},
 })
 
 const ProjectPreview = ({ title, subtitle }: Props) => {
-	const { imageStyle, boxStyle, titleGridContainer } = useStyles()
+	const {
+		imageStyle,
+		boxStyle,
+		titleGridContainer,
+		buttonContainer,
+	} = useStyles()
 
 	return (
 		<>
@@ -46,6 +63,15 @@ const ProjectPreview = ({ title, subtitle }: Props) => {
 				</Grid>
 				<Grid item>
 					<Typography variant='h5'>{subtitle}</Typography>
+				</Grid>
+			</Grid>
+
+			<Grid container className={buttonContainer}>
+				<Grid item component={Button}>
+					visit {title}
+				</Grid>
+				<Grid item component={Button}>
+					Learn more
 				</Grid>
 			</Grid>
 		</>
