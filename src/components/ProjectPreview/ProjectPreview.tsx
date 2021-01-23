cbae970a86ffc82c629360e56b18b5bfc634fdad
Anjan18 <anjanbiber@gmail.com>
2021-01-23 12:40:26 +0600

    project title is added but style is broken

--- src/components/ProjectPreview/ProjectPreview.tsx
+++ src/components/ProjectPreview/ProjectPreview.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
-import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 
 import StyledImage from 'components/Images/StyledImage'
 import MuiLink from 'components/Links/MuiLink'
 import VisitButton from 'components/Buttons/VisitButton'
+import ProjectTitle from './ProjectTitle'
 
 interface Props {
 	title: string
 	subtitle: string
 }
 
-const commonContaienrStyle = {
+export const commonContaienrStyle = {
 	padding: '0 10%',
 }
 
@@ -30,12 +30,6 @@ const useStyles = makeStyles({
 		},
 	},
 
-	titleGridContainer: {
-		...commonContaienrStyle,
-		'& > .MuiGrid-item': {
-			flexBasis: '100%',
-		},
-	},
 	buttonContainer: {
 		...commonContaienrStyle,
 		paddingTop: '2%',
@@ -48,12 +42,7 @@ const useStyles = makeStyles({
 })
 
 const ProjectPreview = ({ title, subtitle }: Props) => {
-	const {
-		imageStyle,
-		boxStyle,
-		titleGridContainer,
-		buttonContainer,
-	} = useStyles()
+	const { imageStyle, boxStyle, buttonContainer } = useStyles()
 
 	return (
 		<>
@@ -61,17 +50,11 @@ const ProjectPreview = ({ title, subtitle }: Props) => {
 				<StyledImage styleClass={imageStyle} />
 			</Box>
 
-			<Grid container className={titleGridContainer}>
-				<Grid item>
-					<Typography variant='h3'>{title}</Typography>
-				</Grid>
-				<Grid item>
-					<Typography variant='h5'>{subtitle}</Typography>
-				</Grid>
-			</Grid>
-
 			<Grid container className={buttonContainer}>
+				<ProjectTitle title={title} subtitle={subtitle} />
+
 				<VisitButton title={title} />
+
 				<MuiLink
 					variant='contained'
 					MuiComponent={Grid}
