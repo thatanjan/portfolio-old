199b9b9da7639ba5a003ca9ae6de01b7e2b2891c
thatanjan <anjan.cules@gmail.com>
2021-01-24 00:58:49 +0600

    all data is organized in an array. and also preview data is also created which will loop through allData array

---
+++ src/apiData/allData.ts
@@ -0,0 +1,22 @@
+import devBook from 'apiData/works/devBook'
+import ApiData from 'apiData/dataClass'
+
+const allData = [devBook]
+
+export default allData
+
+interface NewObject {
+	name: string
+	subtitle: string
+	imagePath: string
+	visitLink: string
+}
+
+export const previewData = allData.forEach((item: ApiData) => {
+	const newObject: NewObject = {
+		name: item.name,
+		subtitle: item.subtitle,
+		imagePath: item.imagePath,
+		visitLink: item.visitLink,
+	}
+})
