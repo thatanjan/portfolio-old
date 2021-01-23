3e9f14d23aed30f06e613f52f017e05d307a22dd
Anjan18 <anjanbiber@gmail.com>
2021-01-23 12:33:28 +0600

    visit button is imported

--- src/components/ProjectPreview/ProjectPreview.tsx
+++ src/components/ProjectPreview/ProjectPreview.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
-import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 
 import StyledImage from 'components/Images/StyledImage'
 import MuiLink from 'components/Links/MuiLink'
+import VisitButton from 'components/Buttons/VisitButton'
 
 interface Props {
 	title: string
@@ -70,16 +71,7 @@ const ProjectPreview = ({ title, subtitle }: Props) => {
 			</Grid>
 
 			<Grid container className={buttonContainer}>
-				<MuiLink
-					MuiComponent={Grid}
-					href='/'
-					item
-					component={Button}
-					variant='contained'
-				>
-					visit {title}
-				</MuiLink>
-
+				<VisitButton title={title} />
 				<MuiLink
 					variant='contained'
 					MuiComponent={Grid}
