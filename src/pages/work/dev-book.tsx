0bc329ea670205e1af21f1ad621a38c7a2610643
Anjan18 <anjanbiber@gmail.com>
2021-01-23 16:24:43 +0600

    a project page called dev book added

---
+++ src/pages/work/dev-book.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Page = () => {
+	return (
+		<>
+			<Head>
+				<title>Dev Book</title>
+			</Head>
+			<div>this is a Dev book</div>
+		</>
+	)
+}
+
+export default Page
