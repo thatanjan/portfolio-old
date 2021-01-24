32c09978471d8b8c00e0c20c77b19d6ec14c09ba
thatanjan <anjan.cules@gmail.com>
2021-01-24 16:40:14 +0600

    data will be sent based on the query parameter

--- src/pages/api/work/[work].ts
+++ src/pages/api/work/[work].ts
@@ -1,6 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import allData from 'apiData/allData'
+import { DEV_BOOK } from 'global/variables'
 
 const workApi = ({ query: { work } }: NextApiRequest, res: NextApiResponse) => {
+	switch (work) {
+		case DEV_BOOK:
+			return res.send(allData[0])
+
+		default:
+			res.send({ msg: 'no Data found' })
+	}
+
 	return res.end(`the api is on ${work}`)
 }
 
