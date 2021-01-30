7c57bbfe47738c896a0a012d356978ca371b0a63
thatanjan <anjan.cules@gmail.com>
2021-01-30 18:22:40 +0600

    preview object data is removed

--- src/apiData/allData.ts
+++ src/apiData/allData.ts
@@ -1,26 +1,6 @@
 import devBook from 'apiData/works/devBook'
 import ApiData from 'apiData/dataClass'
 
-const allData = [devBook]
+const allData: ApiData[] = [devBook]
 
 export default allData
-
-interface NewObject {
-	name: string
-	subtitle: string
-	imagePath: string
-	visitLink: string
-}
-
-export const previewData = allData.forEach(
-	({ name, subtitle, imagePath, visitLink }: ApiData) => {
-		const newObject: NewObject = {
-			name,
-			subtitle,
-			imagePath,
-			visitLink,
-		}
-
-		return newObject
-	}
-)
