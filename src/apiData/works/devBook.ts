b4ab4eb6b9934aec7b9ee98596944c981b448d79
thatanjan <anjan.cules@gmail.com>
2021-01-31 12:42:59 +0600

    data added

--- src/apiData/works/devBook.ts
+++ src/apiData/works/devBook.ts
@@ -1,8 +1,16 @@
-import ApiData from './dataClass'
 import { DEV_BOOK } from 'global/variables'
+import ApiData from './dataClass'
 
 const subtitle = 'social media plattform for developers'
+const imagePath = '/ww.jpg'
+const frontEndTechStack = ['javascript', 'typescript', 'react', 'material-ui']
+const backEndTechStack = ['nodejs', 'express', 'graphql', 'mongodb']
 
-const devBook: ApiData = new ApiData(DEV_BOOK).addSubtitle(subtitle)
+const devBook: ApiData = new ApiData(DEV_BOOK)
+	.addSubtitle(subtitle)
+	.addImage(imagePath)
+	.addVisitLink('/dev-book')
+	.addFrontEndTechStack(frontEndTechStack)
+	.addBackEndTechStack(backEndTechStack)
 
 export default devBook
