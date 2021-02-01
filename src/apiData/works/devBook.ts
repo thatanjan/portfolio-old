41ae1f91de6b1e0b459c53db00894a8c7f90a73f
thatanjan <anjan.cules@gmail.com>
2021-02-01 16:17:53 +0600

    description added

--- src/apiData/works/devBook.ts
+++ src/apiData/works/devBook.ts
@@ -6,6 +6,8 @@ const imagePath = '/ww.jpg'
 const frontEndTechStack = ['javascript', 'typescript', 'react', 'material-ui']
 const backEndTechStack = ['nodejs', 'express', 'graphql', 'mongodb']
 const visitLink = 'https://dev-book.vercel.app'
+const description =
+	'A fully developed social media application built on react and nodejs. It has authentication system where users can login to their account.'
 
 const devBook: ApiData = new ApiData(DEV_BOOK)
 	.addSubtitle(subtitle)
@@ -13,5 +15,6 @@ const devBook: ApiData = new ApiData(DEV_BOOK)
 	.addVisitLink(visitLink)
 	.addFrontEndTechStack(frontEndTechStack)
 	.addBackEndTechStack(backEndTechStack)
+	.addDescription(description)
 
 export default devBook
