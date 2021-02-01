7c83e4986a6002571a6add8e0a0b803dd666cfde
thatanjan <anjan.cules@gmail.com>
2021-02-01 16:06:41 +0600

    visit link updated

--- src/apiData/works/devBook.ts
+++ src/apiData/works/devBook.ts
@@ -5,11 +5,12 @@ const subtitle = 'social media plattform for developers'
 const imagePath = '/ww.jpg'
 const frontEndTechStack = ['javascript', 'typescript', 'react', 'material-ui']
 const backEndTechStack = ['nodejs', 'express', 'graphql', 'mongodb']
+const visitLink = 'https://dev-book.vercel.app'
 
 const devBook: ApiData = new ApiData(DEV_BOOK)
 	.addSubtitle(subtitle)
 	.addImage(imagePath)
-	.addVisitLink('/dev-book')
+	.addVisitLink(visitLink)
 	.addFrontEndTechStack(frontEndTechStack)
 	.addBackEndTechStack(backEndTechStack)
 
