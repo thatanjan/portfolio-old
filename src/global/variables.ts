e7164934bac5f6f349e4252d95738068dad898e5
thatanjan <anjan.cules@gmail.com>
2021-02-01 14:06:00 +0600

    image source variables are added

--- src/global/variables.ts
+++ src/global/variables.ts
@@ -2,3 +2,7 @@ export const DEV_BOOK = 'dev-book'
 
 // eslint-disable-next-line
 export const ifProd = process.env.NODE_ENV === 'production' ? true : false
+
+// image sources
+export const AVATAR = '/myImage.jpg'
+export const LAYOUT_IMAGE = '/myImageLayout.png'
