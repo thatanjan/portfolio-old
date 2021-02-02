de510969119d89fc8c586a507a2686faec26311a
thatanjan <anjan.cules@gmail.com>
2021-02-02 08:09:44 +0600

    some style added to the description

--- src/components/ProjectPreview/ProjectDescription.tsx
+++ src/components/ProjectPreview/ProjectDescription.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Paper from '@material-ui/core/Paper'
+import { makeStyles } from '@material-ui/core/styles'
 
 interface Props {
 	description: string
 }
 
+const useStyles = makeStyles({
+	descriptionStyle: {
+		padding: '0 1rem',
+		marginTop: '3%',
+		fontSize: '1.3rem',
+	},
+})
+
 const ProjectDescription = ({ description }: Props) => {
-	return <Paper>{description}</Paper>
+	const { descriptionStyle } = useStyles()
+
+	return <Paper className={descriptionStyle}>{description}</Paper>
 }
 
 export default ProjectDescription
