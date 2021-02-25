a4e53f0bb25b3018dd1e191497bc68a76a59c42f
thatanjan <anjan.cules@gmail.com>
2021-02-25 08:41:19 +0600

    TechnologiesSection component is created. which will show dynamicly a single type of tech stack

--- src/components/ProjectPreview/ProjectTechnologies.tsx
+++ src/components/ProjectPreview/ProjectTechnologies.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Paper from '@material-ui/core/Paper'
 import Chip from '@material-ui/core/Chip'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+import { nanoid } from 'nanoid'
 
 import SectionTitle from './SectionTitle'
 
@@ -9,14 +12,41 @@ interface Props {
 	backEndTechStack: [string]
 }
 
+interface TechnologiesSectionProps {
+	techStack: [string]
+	endType: string
+}
+
+const TechnologiesSection = ({
+	techStack,
+	endType,
+}: TechnologiesSectionProps) => {
+	return (
+		<Box>
+			<Typography variant='h5'>{endType} End</Typography>
+
+			{techStack.map(item => (
+				<Chip key={nanoid()} color='secondary' label={item} />
+			))}
+		</Box>
+	)
+}
+
 const ProjectTechnologies = ({
 	frontEndTechStack,
 	backEndTechStack,
 }: Props) => {
+	const createSectionProps = (endType: string, techStack: [string]) => ({
+		endType,
+		techStack,
+	})
+
 	return (
 		<Paper>
 			<SectionTitle text='technolgies used' />
-			<Chip color='secondary' label='JavaScript' />
+
+			<TechnologiesSection {...createSectionProps('front', frontEndTechStack)} />
+			<TechnologiesSection {...createSectionProps('back', backEndTechStack)} />
 		</Paper>
 	)
 }
