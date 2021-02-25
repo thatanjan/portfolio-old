328e5ea74ad95aba8dd712de4dbae74d61262468
thatanjan <anjan.cules@gmail.com>
2021-02-25 07:28:26 +0600

    styles added to the title component

--- src/components/ProjectPreview/ProjectTechnologies.tsx
+++ src/components/ProjectPreview/ProjectTechnologies.tsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
+import Chip from '@material-ui/core/Chip'
+import { makeStyles } from '@material-ui/core/styles'
+
+import { titleStyle } from './ProjectDescription'
 
 interface Props {
 	frontEndTechStack: [string]
 	backEndTechStack: [string]
 }
 
+const useStyles = makeStyles({ titleStyle })
+
 const ProjectTechnologies = ({
 	frontEndTechStack,
 	backEndTechStack,
 }: Props) => {
-	console.log(backEndTechStack)
+	const { titleStyle: title } = useStyles()
+
 	return (
 		<Paper>
-			<Typography variant='h4'>Technolgies Used</Typography>
+			<Typography variant='h4' className={title}>
+				Technolgies Used
+			</Typography>
+			<Chip color='secondary' label='JavaScript' />
 		</Paper>
 	)
 }
