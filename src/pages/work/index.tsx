ba842d168856ff359504f30b21135a28599ec846
thatanjan <anjan.cules@gmail.com>
2021-02-25 18:09:04 +0600

    fetches data from '/api/work'. pass the data as props

--- src/pages/work/index.tsx
+++ src/pages/work/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
+import { GetStaticProps } from 'next'
+import { ifProd } from 'global/variables'
+
+import ApiData from 'apiData/works/dataClass'
 
 const ProjectPreview = dynamic(
 	() => import('components/ProjectPreview/ProjectPreview')
@@ -24,3 +28,14 @@ const Work = () => {
 }
 
 export default Work
+
+export const getStaticProps: GetStaticProps = async () => {
+	const url = ifProd
+		? `${process.env.API_URL}/api/work`
+		: `http://localhost:3000/api/work`
+
+	const res = await fetch(url)
+	const data = await res.json()
+
+	return { props: { data } }
+}
