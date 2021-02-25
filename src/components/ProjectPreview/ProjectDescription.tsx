3a33c3b34226f544e2fd2a67ff1638216d6a8b34
thatanjan <anjan.cules@gmail.com>
2021-02-25 07:17:17 +0600

    title component added to the description. with styles

--- src/components/ProjectPreview/ProjectDescription.tsx
+++ src/components/ProjectPreview/ProjectDescription.tsx
@@ -1,23 +1,39 @@
 import React from 'react'
 import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
 
 interface Props {
 	description: string
 }
 
+export const commonPadding = `1rem`
+
 const useStyles = makeStyles({
 	descriptionStyle: {
-		padding: '1rem',
-		margin: '5% 0',
+		padding: commonPadding,
 		fontSize: '1.3rem',
+	},
+	title: {
+		padding: commonPadding,
+		paddingBottom: '0',
+	},
+	descriptionContainer: {
+		marginBottom: '5%',
 	},
 })
 
 const ProjectDescription = ({ description }: Props) => {
-	const { descriptionStyle } = useStyles()
+	const { title, descriptionStyle, descriptionContainer } = useStyles()
 
-	return <Paper className={descriptionStyle}>{description}</Paper>
+	return (
+		<Paper className={descriptionContainer}>
+			<Typography className={title} variant='h4'>
+				Description
+			</Typography>
+			<Paper className={descriptionStyle}>{description}</Paper>
+		</Paper>
+	)
 }
 
 export default ProjectDescription
