58ace3b1d38e48ef525c260c2cd5c7812e8d75c8
thatanjan <anjan.cules@gmail.com>
2021-03-01 16:28:07 +0600

    avatar is loaded lazily

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import dynamic from 'next/dynamic'
 import Head from 'next/head'
-import Avatar from 'components/Avatar/Avatar'
 import useLargerThanLG from 'hooks/useLargerThanLG'
 import AboutMe from 'components/AboutMe/AboutMe'
+
+const Avatar = dynamic(() => import('components/Avatar/Avatar'))
 
 const Home = () => {
 	const largerThanLG = useLargerThanLG()
