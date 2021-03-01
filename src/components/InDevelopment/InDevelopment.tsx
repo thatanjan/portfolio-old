cd7cdb960cc1500d2a436ae1860aa714998bc572
thatanjan <anjan.cules@gmail.com>
2021-03-01 09:25:02 +0600

    a component to show a page is in development

---
+++ src/components/InDevelopment/InDevelopment.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+const InDevelopment = () => {
+	return <Typography variant='h1'>This page is under development</Typography>
+}
+
+export default InDevelopment
