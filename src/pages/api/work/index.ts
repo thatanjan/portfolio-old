ca179d9aca722cc4bdecf1eb4d804d6fc06ecc3a
thatanjan <anjan.cules@gmail.com>
2021-03-01 09:40:55 +0600

    no need of api routes

--- src/pages/api/work/index.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import allData from 'apiData/works/allData'
-
-const workApi = (_: NextApiRequest, res: NextApiResponse) => res.send(allData)
-
-export default workApi
