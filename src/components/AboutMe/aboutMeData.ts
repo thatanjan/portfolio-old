7d2b691f6d73a1818d04cbc127b3d2c565e4eb04
thatanjan <anjan.cules@gmail.com>
2021-03-03 17:29:02 +0600

    new about me objects are created and exported in an array

--- src/components/AboutMe/aboutMeData.ts
+++ src/components/AboutMe/aboutMeData.ts
@@ -8,3 +8,11 @@ export class Data {
 		this.answer = answer
 	}
 }
+
+const WHO_AM_I_ANSWER = 'my name is anjan'
+
+const whoAmI = new Data('who am I?', WHO_AM_I_ANSWER)
+
+const aboutMeData: Data[] = [whoAmI]
+
+export default aboutMeData
