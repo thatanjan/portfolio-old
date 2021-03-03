732509df90d25e144a70c581e8013b54b6b9fb59
thatanjan <anjan.cules@gmail.com>
2021-03-03 18:03:37 +0600

    new datas are added to the aboutMeData array

--- src/components/AboutMe/aboutMeData.ts
+++ src/components/AboutMe/aboutMeData.ts
@@ -9,10 +9,35 @@ export class AboutMeData {
 	}
 }
 
-const WHO_AM_I_ANSWER = 'my name is anjan'
+const WHO_AM_I_ANSWER = 'my name is anjan. I am a Full stack developer.'
 
-const whoAmI = new AboutMeData('who am I?', WHO_AM_I_ANSWER)
+const WHAT_PROBLEMS_DO_I_SOLVE: string =
+	'I can create complex full stack web applications like social media application, blogging, e-commerce website and many more.'
 
-const aboutMeData: AboutMeData[] = [whoAmI]
+const WHAT_HAVE_I_DONE: string =
+	'I have developed and currently maintaining an social media application called Confession.'
+
+const WHY_DO_I_DO_WHAT_I_DO = 'I love to solve problems and develop new ideas.'
+
+const whoAmI = new AboutMeData('who am I', WHO_AM_I_ANSWER)
+
+const whatProblemsDoISolve = new AboutMeData(
+	'what problems do I solve',
+	WHAT_PROBLEMS_DO_I_SOLVE
+)
+
+const whyDoIDoWhatIDo = new AboutMeData(
+	'why do I do what I do',
+	WHY_DO_I_DO_WHAT_I_DO
+)
+
+const whatHaveIDone = new AboutMeData('what have I done', WHAT_HAVE_I_DONE)
+
+const aboutMeData: AboutMeData[] = [
+	whoAmI,
+	whatProblemsDoISolve,
+	whatHaveIDone,
+	whyDoIDoWhatIDo,
+]
 
 export default aboutMeData
