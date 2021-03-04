e6c6f9790409c2c18de95d955e5ff217b1bf73c9
thatanjan <anjan.cules@gmail.com>
2021-03-04 12:46:45 +0600

    contact list is added to the contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import InDevelopment from 'components/InDevelopment/InDevelopment'
+import ContactList from 'components/Lists/ContactList'
 
 interface Props {}
 
 const Work = (props: Props) => {
-	return <InDevelopment />
+	return (
+		<>
+			{' '}
+			<ContactList />{' '}
+		</>
+	)
 }
 
 export default Work
