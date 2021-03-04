637c544c9a73585b3883271ab1bbe21538d24398
thatanjan <anjan.cules@gmail.com>
2021-03-04 12:34:31 +0600

    new contact is created from contacts and exported them in array

--- src/components/Lists/ContactListData.ts
+++ src/components/Lists/ContactListData.ts
@@ -1,3 +1,9 @@
+import Instagram from '../../../public/svgs/social_media/instagram.svg'
+import Linkedin from '../../../public/svgs/social_media/linkedin.svg'
+import Facebook from '../../../public/svgs/social_media/facebook.svg'
+import Twitter from '../../../public/svgs/social_media/twitter.svg'
+import Youtube from '../../../public/svgs/social_media/youtube.svg'
+
 class Contact {
 	name: string
 
@@ -19,3 +25,23 @@ class Contact {
 		return this
 	}
 }
+
+const facebook = new Contact('facebook')
+	.addIcon(Facebook)
+	.addLink('https://www.facebook.com/thatanjan/')
+const instagram = new Contact('instagram')
+	.addIcon(Instagram)
+	.addLink('https://www.instagram.com/thatanjan/')
+const linkedin = new Contact('Linkedin')
+	.addIcon(Linkedin)
+	.addLink('https://www.linkedin.com/in/thatanjan/')
+const twitter = new Contact('twitter')
+	.addIcon(Twitter)
+	.addLink('https://www.twitter.com/thatanjan/')
+const youtube = new Contact('youtube')
+	.addIcon(Youtube)
+	.addLink('https://www.youtube.com/channel/UCBaGowNYTUsm3IDaHbLRMYw')
+
+const allContacts: Contact[] = [linkedin, twitter, instagram, youtube, facebook]
+
+export default allContacts
