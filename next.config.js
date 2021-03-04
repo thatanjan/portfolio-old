4b082058eb5133a285ee5697e9336eddc8ee5d97
thatanjan <anjan.cules@gmail.com>
2021-03-04 11:45:13 +0600

    svg loader config added

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@ const withMDX = require('@next/mdx')({
 	extension: /\.mdx$/,
 })
 
+const svgLoader = {
+	test: /\.svg$/,
+	use: ['@svgr/webpack'],
+}
+
 module.exports = withMDX({
 	pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+	webpack(config) {
+		config.module.rules.push(svgLoader)
+		return config
+	},
 })
