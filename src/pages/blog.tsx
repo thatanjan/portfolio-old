b419b2eeca29f97866bfa25bc9142f601573e304
thatanjan <anjan.cules@gmail.com>
2021-03-08 17:14:11 +0600

    banner image for blog is added

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import InDevelopment from 'components/InDevelopment/InDevelopment'
+import { makeStyles } from '@material-ui/core/styles'
+import Box from '@material-ui/core/Box'
+import Image from 'next/image'
 
 interface Props {}
 
+const useStyles = makeStyles({
+	root: {},
+})
+
 const Work = (props: Props) => {
-	return <InDevelopment />
+	const classes = useStyles()
+
+	return (
+		<Box className={classes.root}>
+			<Image
+				layout='intrinsic'
+				src='/cules_banner.jpg'
+				height={600}
+				width={1920}
+				objectFit='cover'
+			/>
+		</Box>
+	)
 }
 
 export default Work
