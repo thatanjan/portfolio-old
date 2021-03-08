8c01d1e6b86505b51c9b6f2bd13afde6635dcac1
thatanjan <anjan.cules@gmail.com>
2021-03-08 18:17:05 +0600

    Blog page will be sepreated into two parts when screen is wider than md

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import { ReactNode } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
 import Image from 'next/image'
 import PostPreview from 'components/Posts/PostPreview'
 
@@ -8,7 +9,18 @@ interface Props {}
 
 const useStyles = makeStyles({
 	root: {},
+	gridContainer: {},
 })
+
+interface GridItemProps {
+	children: ReactNode
+}
+
+const GridItem = ({ children }: GridItemProps) => (
+	<Grid item md={6} xs={12}>
+		{children}
+	</Grid>
+)
 
 const Work = (props: Props) => {
 	const classes = useStyles()
@@ -24,7 +36,10 @@ const Work = (props: Props) => {
 					objectFit='cover'
 				/>
 			</Box>
-			<PostPreview />
+			<Grid container>
+				<GridItem />
+				<GridItem />
+			</Grid>
 		</>
 	)
 }
