7560bba40f0c881464566506e3cd32547720b9bd
thatanjan <anjan.cules@gmail.com>
2021-04-29 16:08:01 +0600

    this file will export bunch of objects containing tech name and their avatar.

---
+++ src/apiData/works/Techs.ts
@@ -0,0 +1,50 @@
+export class Tech {
+	name: string
+
+	avatar: string
+
+	constructor(name: string, avatar: string) {
+		this.name = name
+		this.avatar = avatar
+	}
+}
+
+export const javascript = new Tech(
+	'Javascript',
+	'https://web.archive.org/web/20190831204424if_/https://2ch.pm/b/thumb/202995921/15672793792330s.jpg'
+)
+
+export const react = new Tech(
+	'ReactJS',
+	'https://cdn.auth0.com/blog/reactjs16/logo.png'
+)
+
+export const typescript = new Tech(
+	'TypeScript',
+	'https://authfix-le-gaulois.tech/wp-content/uploads/2019/03/ts-320x320.png'
+)
+
+export const mui = new Tech(
+	'Material UI',
+	'https://i.pinimg.com/236x/e4/9d/a7/e49da7a0eeedfa732124d9aabeae3062.jpg?nii=t'
+)
+
+export const nodejs = new Tech(
+	'NodeJS',
+	'https://www.linux.org/data/avatars/o/78/78445.jpg?1554259998'
+)
+
+export const graphql = new Tech(
+	'Graphql',
+	'https://2.bp.blogspot.com/-5F6wnAKb3Dk/XDaOYcVj5NI/AAAAAAAAUUY/qsSuYCPfBnIS0c54WDLCUy4c-PNdHaSPgCLcBGAs/s640/Complete%2Bguide%2Bto%2Bbuilding%2Ba%2BGraphQL%2BAPI.jpg'
+)
+
+export const express = new Tech(
+	'ExpressJS',
+	'https://dz8fbjd9gwp2s.cloudfront.net/orgData/5e85a31d0cf2fc4c81d60c2f/pages/assets/images/II7lBexpress.png'
+)
+
+export const mongodb = new Tech(
+	'MongoDB',
+	'https://onmogul.s3.amazonaws.com/uploads/user/avatar/3077013/4bf54bb806.png'
+)
