f9292138f178f83e60b8698bd21f4889c1672569
thatanjan <anjan.cules@gmail.com>
2021-04-29 16:17:49 +0600

    Tech stacks are update in all projects with Tech Class objects

--- src/apiData/works/confession.ts
+++ src/apiData/works/confession.ts
@@ -1,11 +1,22 @@
 import { CONFESSION } from 'global/variables'
 import ApiData from './dataClass'
+import {
+	Tech,
+	javascript,
+	typescript,
+	react,
+	mui,
+	nodejs,
+	express,
+	graphql,
+	mongodb,
+} from './Techs'
 
 const subtitle =
 	'social media plattform for people to overcome their imposter syndrome'
 const imagePath = '/project__confession.png'
-const frontEndTechStack = ['javascript', 'typescript', 'react', 'material-ui']
-const backEndTechStack = ['nodejs', 'express', 'graphql', 'mongodb']
+const frontEndTechStack: Tech[] = [javascript, typescript, react, mui]
+const backEndTechStack: Tech[] = [nodejs, express, graphql, mongodb]
 const visitLink = 'https://confession.vercel.app'
 const description =
 	'A fully developed social media application built on react and nodejs. It has authentication system where users can login to their account.'
--- src/apiData/works/portofolio.ts
+++ src/apiData/works/portofolio.ts
@@ -1,11 +1,12 @@
 import ApiData from './dataClass'
+import { Tech, javascript, typescript, react, mui } from './Techs'
 
 const PORTOFOLIO = 'portofolio'
 
 const subtitle = 'Portofolio website of Anjan Shomodder'
 
 const imagePath = '/project__portofolio.png'
-const frontEndTechStack = ['javascript', 'typescript', 'react', 'material-ui']
+const frontEndTechStack: Tech[] = [javascript, typescript, react, mui]
 const visitLink = 'https://anjan.vercel.app'
 const description = 'A fully developed portofolio website built on reactjs.'
 
