4da9151a1ab0206b8fcf96ab7d6dd7bbe6051e06
thatanjan <anjan.cules@gmail.com>
2021-04-29 17:54:02 +0600

    contact item icons will be a link to a image not a svg. obiously icon links are replaced with svg

--- src/components/Lists/ContactListData.ts
+++ src/components/Lists/ContactListData.ts
@@ -1,15 +1,7 @@
-import { ElementType } from 'react'
-import Instagram from '../../../public/svgs/social_media/instagram.svg'
-import Linkedin from '../../../public/svgs/social_media/linkedin.svg'
-import Facebook from '../../../public/svgs/social_media/facebook.svg'
-import Twitter from '../../../public/svgs/social_media/twitter.svg'
-import Youtube from '../../../public/svgs/social_media/youtube.svg'
-import Gmail from '../../../public/svgs/social_media/gmail.svg'
-
 export class Contact {
 	name: string
 
-	Icon: ElementType
+	Icon: string
 
 	link: string
 
@@ -17,7 +9,7 @@ export class Contact {
 		this.name = name
 	}
 
-	addIcon(icon: ElementType) {
+	addIcon(icon: string) {
 		this.Icon = icon
 		return this
 	}
@@ -31,29 +23,32 @@ export class Contact {
 const USER_NAME = 'thatanjan'
 
 const facebook = new Contact(USER_NAME)
-	.addIcon(Facebook)
+	.addIcon(
+		'https://www.clipartmax.com/png/middle/99-999735_sam-linder-honda-salinas-transparent-background-facebook-logo-png.png'
+	)
 	.addLink('https://www.facebook.com/thatanjan/')
 const instagram = new Contact(USER_NAME)
-	.addIcon(Instagram)
+	.addIcon(
+		'https://3.bp.blogspot.com/-HZrSnSEH36k/V1NM6uUmaPI/AAAAAAAAAG4/SbuRxuq0HGQhqBs3m1AvegQea1OwdYRyQCLcB/s320/instagram-new-logo.jpg'
+	)
 	.addLink('https://www.instagram.com/thatanjan/')
 const linkedin = new Contact(USER_NAME)
-	.addIcon(Linkedin)
+	.addIcon(
+		'https://www.digitalmarketingpta.co.za/wp-content/uploads/2017/05/linkedin-piyagroup-pretoria.jpg'
+	)
 	.addLink('https://www.linkedin.com/in/thatanjan/')
 const twitter = new Contact(USER_NAME)
-	.addIcon(Twitter)
+	.addIcon(
+		'https://www.thenicheng.com/wp-content/uploads/2018/05/twitter-icon.png'
+	)
 	.addLink('https://www.twitter.com/thatanjan/')
-const youtube = new Contact('Cules Coding')
-	.addIcon(Youtube)
-	.addLink('https://www.youtube.com/channel/UCBaGowNYTUsm3IDaHbLRMYw')
-const gmail = new Contact('anjan.cules@gmail.com').addIcon(Gmail).addLink('')
 
-const allContacts: Contact[] = [
-	gmail,
-	linkedin,
-	twitter,
-	instagram,
-	youtube,
-	facebook,
-]
+const gmail = new Contact('anjan.cules@gmail.com')
+	.addIcon(
+		'https://1.bp.blogspot.com/-Q8pkiM6WmIM/WMNjlRdRLrI/AAAAAAAADhI/nmkOC-CiC6Y24RiblhzDKovYxfZXdRl2wCLcB/s400/google-gmail.jpg'
+	)
+	.addLink('')
+
+const allContacts: Contact[] = [gmail, linkedin, twitter, instagram, facebook]
 
 export default allContacts
