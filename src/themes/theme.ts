665a9401833047e363fb63050b9568f4da9c69e3
thatanjan <anjan.cules@gmail.com>
2021-06-12 08:57:35 +0600

    font size on media queries is removed from the theme

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -19,27 +19,10 @@ const darkTheme = createMuiTheme({
 
 const theme = responsiveFontSizes(darkTheme)
 
-theme.typography.subtitle1 = {
-	...theme.typography.subtitle1,
-	[theme.breakpoints.up('sm')]: {
-		fontSize: '1.3rem',
-	},
-}
-
 theme.typography.button = {
 	...theme.typography.button,
 	[theme.breakpoints.down('xs')]: {
 		fontSize: '0.775rem',
-	},
-}
-
-theme.typography.body1 = {
-	...theme.typography.body1,
-	[theme.breakpoints.up('sm')]: {
-		fontSize: '1.3rem',
-	},
-	[theme.breakpoints.up('md')]: {
-		fontSize: '1.5rem',
 	},
 }
 
