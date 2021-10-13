f3e921feedd9e9ae16be562fb67c8db11828f22e
thatanjan <thatanjan@gmail.com>
2021-10-13 13:08:17 +0600

    layout Image will take different amount of height based on the screen size

--- src/components/PageLayout/LayoutImage.tsx
+++ src/components/PageLayout/LayoutImage.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, Theme } from '@material-ui/core/styles'
 
 import StyledImage from 'components/Images/StyledImage'
 import { LAYOUT_IMAGE } from 'global/variables'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => ({
 	imageStyle: {},
 	containerStyle: {
 		width: '100%',
+		height: '100vh',
 		display: 'grid',
 		alignItems: 'end',
 		'& > div': {
 			height: 'inherit',
 		},
+		[theme.breakpoints.down('xs')]: {
+			height: '80vh',
+		},
 	},
-})
+}))
 
 const LayoutImage = () => {
 	const { imageStyle, containerStyle } = useStyles()
