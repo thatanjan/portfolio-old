51fff42750e623b0edaef67c21c397c7b7c47a81
thatanjan <thatanjan@gmail.com>
2021-10-13 13:34:03 +0600

    A title will be shown under the layout image  to make it more beautiful. Title will only be shown if screen size is smaller than md.

--- src/components/PageLayout/LayoutImage.tsx
+++ src/components/PageLayout/LayoutImage.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
 import { makeStyles, Theme } from '@material-ui/core/styles'
 
 import StyledImage from 'components/Images/StyledImage'
+import useLargerThanMD from 'hooks/useLargerThanMD'
 import { LAYOUT_IMAGE } from 'global/variables'
 
 const useStyles = makeStyles((theme: Theme) => ({
 	imageStyle: {},
+
 	containerStyle: {
 		width: '100%',
 		height: '100vh',
@@ -18,16 +22,40 @@ const useStyles = makeStyles((theme: Theme) => ({
 			height: '80vh',
 		},
 	},
+	outerParent: {
+		position: 'relative',
+	},
+	titleContainer: {
+		position: 'absolute',
+		bottom: 0,
+		background: theme.palette.background.paper,
+		width: '100%',
+		height: '10rem',
+		display: 'grid',
+		placeItems: 'center',
+		borderRadius: '20rem 20rem 0 0',
+	},
 }))
 
 const LayoutImage = () => {
-	const { imageStyle, containerStyle } = useStyles()
+	const { imageStyle, containerStyle, outerParent, titleContainer } = useStyles()
+
+	const largerThanMD = useLargerThanMD()
 
 	return (
 		<>
-			<div className={containerStyle}>
-				<StyledImage imagePath={LAYOUT_IMAGE} styleClass={imageStyle} />
-			</div>
+			<Box className={outerParent}>
+				<div className={containerStyle}>
+					<StyledImage imagePath={LAYOUT_IMAGE} styleClass={imageStyle} />
+				</div>
+				{!largerThanMD && (
+					<Box className={titleContainer}>
+						<Typography variant='h2' align='center'>
+							Anjan <br /> Shomodder
+						</Typography>
+					</Box>
+				)}
+			</Box>
 		</>
 	)
 }
