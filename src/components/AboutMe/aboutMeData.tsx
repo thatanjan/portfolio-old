2a914a26e83b239347f4114b8c5fdf75b8ddf9dc
thatanjan <thatanjan@gmail.com>
2021-10-14 09:41:27 +0600

    About me datas are changed

--- src/components/AboutMe/aboutMeData.tsx
+++ src/components/AboutMe/aboutMeData.tsx
@@ -36,28 +36,46 @@ export class AboutMeData {
 	}
 }
 
-const WHO_AM_I_ANSWER = 'my name is anjan. I am a Full stack developer.'
+const WHO_AM_I_ANSWER = () => (
+	<Typography variant='body1' component='h2' style={{ display: 'inline' }}>
+		{' '}
+		My name is anjan. I am a Full stack developer. I also write Blogs on{' '}
+		<SpecialLink href='https://www.culescoding.space/'>
+			culescoding.space
+		</SpecialLink>{' '}
+		I run a youtube channel called{' '}
+		<SpecialLink href='https://www.youtube.com/channel/UCBaGowNYTUsm3IDaHbLRMYw?sub_confirmation=1'>
+			Cules Coding
+		</SpecialLink>{' '}
+		where I teach people full stack web development, data structure, alghorithms
+		and many more.
+	</Typography>
+)
 
 const WHAT_PROBLEMS_DO_I_SOLVE: string =
-	'I can create complex full stack web applications like social media application, blogging, e-commerce website and many more.'
+	'I can develop complex full stack web applications like social media application, blogging, e-commerce website and many more.'
 
 const WHAT_HAVE_I_DONE = () => {
 	return (
 		<>
 			<Typography variant='body1' component='h2' style={{ display: 'inline' }}>
 				I have developed and currently maintaining an social media application
-				called Confession. I have also created other cool projects.{' '}
+				called{' '}
+				<SpecialLink href='https://con-fession.vercel.app/'>Confession</SpecialLink>
+				to help people overcome their imposter syndrome. I have also created other
+				cool projects.{' '}
 			</Typography>
 
 			<SpecialLink href='/work'>
-				{makeFirstLetterUpperCase('see my work here')}
+				{makeFirstLetterUpperCase('see my work from here')}
 			</SpecialLink>
 		</>
 	)
 }
-const WHY_DO_I_DO_WHAT_I_DO = 'I love to solve problems and develop new ideas.'
+const WHY_DO_I_DO_WHAT_I_DO =
+	'I love to solve problems, develop new ideas and apply them on real life. I want to contribute to the world for greater good. I believe science and technolgy will allow me to do that.'
 
-const whoAmI = new AboutMeData('who am I', WHO_AM_I_ANSWER)
+const whoAmI = new AboutMeData('who am I', '').addReactNode(<WHO_AM_I_ANSWER />)
 
 const whatProblemsDoISolve = new AboutMeData(
 	'what problems do I solve',
