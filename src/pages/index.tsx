89c0bd61e52078755e476638f222c25ad8c85859
thatanjan <thatanjan@gmail.com>
2021-11-21 20:23:42 +0600

    profile image has added to home page with some content. but need to fix layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,33 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import React from 'react'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
 
-export default function Home() {
-	return <h1>hello world</h1>
+import ProfileImage from 'components/Images/ProfileImage'
+
+const Index = () => {
+	return (
+		<Grid
+			container
+			sx={{ minHeight: '100vh' }}
+			justifyContent='center'
+			alignItems='center'
+		>
+			<Grid
+				item
+				xs={10}
+				sx={{ display: 'grid', placeItems: 'center', alignSelf: 'flex-end' }}
+			>
+				<ProfileImage />
+			</Grid>
+			<Grid item xs={10} sx={{ alignSelf: 'baseline' }}>
+				<Typography>
+					I'm a Freelance UI/UX Designer and Developer based in London, England. I
+					strives to build immersive and beautiful web applications through carefully
+					crafted code and user-centric design.
+				</Typography>
+			</Grid>
+		</Grid>
+	)
 }
+
+export default Index
