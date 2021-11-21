633275954298336dee74011c4bec06620b3c7c61
thatanjan <thatanjan@gmail.com>
2021-11-21 23:46:44 +0600

    Index page layout is fixed with css grid. But is styled for only small sizes. not for other screen sizes.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,46 @@
 import React from 'react'
-import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
 
 import ProfileImage from 'components/Images/ProfileImage'
 
 const Index = () => {
 	return (
-		<Grid
-			container
-			sx={{ minHeight: '100vh' }}
-			justifyContent='center'
-			alignItems='center'
+		<Box
+			sx={{
+				display: 'grid',
+				height: '100vh',
+				gridTemplateColumns: {
+					xs: '1fr',
+				},
+				gridTemplateRows: {
+					xs: '1fr 1fr',
+				},
+				rowGap: {
+					xs: '2rem',
+				},
+			}}
 		>
-			<Grid
-				item
-				xs={10}
-				sx={{ display: 'grid', placeItems: 'center', alignSelf: 'flex-end' }}
+			<Box
+				sx={{
+					display: 'grid',
+					placeItems: {
+						xs: 'end center',
+					},
+				}}
 			>
 				<ProfileImage />
-			</Grid>
-			<Grid item xs={10} sx={{ alignSelf: 'baseline' }}>
+			</Box>
+			<Box>
 				<Typography>
-					I'm a Freelance UI/UX Designer and Developer based in London, England. I
-					strives to build immersive and beautiful web applications through carefully
-					crafted code and user-centric design.
+					Maiores eius voluptatum ipsa nesciunt illo sunt ratione fugiat dignissimos
+					odio. Repudiandae dolore a consectetur autem non? Quasi vero corporis
+					quibusdam rem doloremque in. Deleniti quidem ea recusandae alias nostrum
+					Deleniti atque recusandae tempore fuga iste alias Consequuntur saepe sequi
+					maiores sunt molestiae aut fugit.
 				</Typography>
-			</Grid>
-		</Grid>
+			</Box>
+		</Box>
 	)
 }
 
