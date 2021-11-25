e40f7beb10837f0ea77ecda3bcea34c3c93c12fb
thatanjan <thatanjan@gmail.com>
2021-11-25 20:39:39 +0600

    ImageSlideShow component is created. It will create a slideshow of image. Mainly for project screenshots

---
+++ src/components/Slides/ImageSlideShow.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import Image from 'next/image'
+import { styled } from '@mui/system'
+import Box from '@mui/material/Box'
+import { nanoid } from 'nanoid'
+import { Swiper, SwiperSlide, SwiperProps } from 'swiper/react'
+import SwiperCore, { Pagination, Navigation } from 'swiper'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+
+SwiperCore.use([Pagination, Navigation])
+
+const CustomSwiperContainer = styled(Swiper)<SwiperProps>(({ theme }) => ({
+	paddingBottom: '3rem',
+	'& .swiper-pagination-bullet': {
+		background: 'white',
+	},
+	'& .swiper-button-next:after': {
+		fontSize: '2rem !important',
+		color: theme.palette.secondary.main,
+	},
+	'& .swiper-button-prev:after': {
+		fontSize: '2rem !important',
+		color: theme.palette.secondary.main,
+	},
+}))
+
+interface Props {
+	images: string[]
+}
+
+const ProjectImageSlideShow = ({ images }: Props) => {
+	return (
+		<Box sx={{ margin: '2rem auto' }}>
+			<CustomSwiperContainer
+				grabCursor
+				pagination={{
+					clickable: true,
+				}}
+				loop
+				autoplay={{
+					delay: 3000,
+				}}
+				navigation
+			>
+				{images.map((imagePath) => (
+					<SwiperSlide key={nanoid()}>
+						<Box>
+							<Image src={imagePath} layout='responsive' width={16} height={9} />
+						</Box>
+					</SwiperSlide>
+				))}
+			</CustomSwiperContainer>
+		</Box>
+	)
+}
+
+export default ProjectImageSlideShow
