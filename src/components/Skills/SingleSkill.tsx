031ff5683b4c47b2af8142a258924cbfb1bdc990
thatanjan <thatanjan@gmail.com>
2021-11-27 00:14:13 +0600

    SingleSkill component will display a single skill including name and image

---
+++ src/components/Skills/SingleSkill.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image from 'next/image'
+import Typography from '@mui/material/Typography'
+
+interface Props {
+	name: string
+	image: string
+}
+
+const SingleSkill = ({ name, image }: Props) => {
+	const dimension = 200
+
+	return (
+		<>
+			<Image src={image} height={dimension} width={dimension} objectFit='cover' />
+
+			<Typography sx={{ textTransform: 'capitalize' }}>{name}</Typography>
+		</>
+	)
+}
+
+export default SingleSkill
