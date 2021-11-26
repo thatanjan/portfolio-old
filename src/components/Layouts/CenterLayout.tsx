09351e56e35457f8aa9e0daa0daeccc39c38a508
thatanjan <thatanjan@gmail.com>
2021-11-26 11:55:40 +0600

    Center layout will make the page layout center with responsiveness.

---
+++ src/components/Layouts/CenterLayout.tsx
@@ -0,0 +1,20 @@
+import Grid from '@mui/material/Grid'
+import React from 'react'
+
+interface Props {
+	children: React.ReactNode
+}
+
+const CenterLayout = ({ children }: Props) => {
+	return (
+		<>
+			<Grid container justifyContent='center'>
+				<Grid item xs={11} md={9} lg={7} xl={6}>
+					{children}
+				</Grid>
+			</Grid>
+		</>
+	)
+}
+
+export default CenterLayout
