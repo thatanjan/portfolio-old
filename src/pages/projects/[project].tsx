223577dd381f005a7faeea1239b2deefa76bb875
thatanjan <thatanjan@gmail.com>
2021-11-27 00:43:19 +0600

    New layout is created for project page. New layout will have 1. Project image , title and description on left 2. Project   info will be on right

--- src/pages/projects/[project].tsx
+++ src/pages/projects/[project].tsx
@@ -9,27 +9,38 @@ interface Props {}
 
 const Project = (props: Props) => {
 	return (
-		<CenterLayout>
-			<ProjectImageSlideShow images={['/anime_19.jpg', '/anime_19.jpg']} />
+		<>
+			<Grid container justifyContent='center'>
+				<Grid item container xs={8} justifyContent='space-between'>
+					<Grid item xs={12}>
+						<ProjectImageSlideShow images={['/anime_19.jpg', '/anime_19.jpg']} />
+					</Grid>
 
-			<Typography variant='h2' gutterBottom>
-				Confession
-			</Typography>
+					<Grid item xs={7}>
+						<Typography variant='h2' gutterBottom>
+							Confession
+						</Typography>
 
-			<Typography gutterBottom>
-				Elit ut eius repellendus nihil facere. Numquam neque asperiores iusto libero
-				fugiat obcaecati sit incidunt Consectetur architecto nesciunt iste modi
-				illum. Recusandae similique fuga provident incidunt praesentium? Sit
-				consectetur cumque cumque aut eaque. Vel voluptatem fugiat quo tempore
-				recusandae dolor, nihil distinctio tenetur? Modi consequuntur magnam tempora
-				repellat quis Saepe laudantium itaque sapiente saepe corporis Facilis soluta
-				atque dolor optio quae numquam. Minima exercitationem in sit perferendis
-				animi a similique Incidunt dicta quis repudiandae quae delectus Tempora
-				obcaecati expedita sed dicta obcaecati inventore. Cum odit impedit explicabo
-				earum iure maiores? Neque adipisci nobis nostrum minus repudiandae? Nostrum
-				distinctio non quis?
-			</Typography>
-		</CenterLayout>
+						<Typography gutterBottom sx={{ lineHeight: 2 }}>
+							Elit ut eius repellendus nihil facere. Numquam neque asperiores iusto
+							libero fugiat obcaecati sit incidunt Consectetur architecto nesciunt iste
+							modi illum. Recusandae similique fuga provident incidunt praesentium? Sit
+							consectetur cumque cumque aut eaque. Vel voluptatem fugiat quo tempore
+							recusandae dolor, nihil distinctio tenetur? Modi consequuntur magnam
+							tempora repellat quis Saepe laudantium itaque sapiente saepe corporis
+							Facilis soluta atque dolor optio quae numquam. Minima exercitationem in
+							sit perferendis animi a similique Incidunt dicta quis repudiandae quae
+							delectus Tempora obcaecati expedita sed dicta obcaecati inventore. Cum
+							odit impedit explicabo earum iure maiores? Neque adipisci nobis nostrum
+							minus repudiandae? Nostrum distinctio non quis?
+						</Typography>
+					</Grid>
+					<Grid item xs={4} pt={10}>
+						<Typography variant='h4'>Project Info</Typography>
+					</Grid>
+				</Grid>
+			</Grid>
+		</>
 	)
 }
 
