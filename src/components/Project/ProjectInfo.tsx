51e1f4baf688b28f6d02b1b64c602db51d4068db
thatanjan <thatanjan@gmail.com>
2021-11-28 18:49:53 +0600

    linke component added if the field value is link .

--- src/components/Project/ProjectInfo.tsx
+++ src/components/Project/ProjectInfo.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Grid from '@mui/material/Grid'
+import { nanoid } from 'nanoid'
 
 import Info from 'classes/Project/ProjectInfo'
 
 import { convertUnderScoreToSpace } from 'utils/convertString'
+
+import TypographyLink from 'components/Links/TypographyLink'
 
 interface Props {
 	infos: Info[]
@@ -12,8 +15,8 @@ interface Props {
 const ProjectInfo = ({ infos }: Props) => {
 	return (
 		<Grid container pt='2rem'>
-			{infos.map(({ field, value }) => (
-				<>
+			{infos.map(({ field, value, href }) => (
+				<Fragment key={nanoid()}>
 					<Grid item xs={12} container sx={{ textTransform: 'capitalize' }}>
 						<Grid item xs={4}>
 							{convertUnderScoreToSpace(field)}
@@ -22,10 +25,10 @@ const ProjectInfo = ({ infos }: Props) => {
 							:
 						</Grid>
 						<Grid item xs={7}>
-							{value}
+							{href ? <TypographyLink href={href}>{value}</TypographyLink> : value}
 						</Grid>
 					</Grid>
-				</>
+				</Fragment>
 			))}
 		</Grid>
 	)
