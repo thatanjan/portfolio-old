0848fce92fda8f0a2a55555eba71644075fbcfef
thatanjan <thatanjan@gmail.com>
2021-11-28 20:21:50 +0600

    techs and tools are used

--- src/data/projects/confession.ts
+++ src/data/projects/confession.ts
@@ -1,6 +1,26 @@
 import Project from 'classes/Project/Project'
 import Info from 'classes/Project/ProjectInfo'
 
+import {
+	javascript,
+	react,
+	redux,
+	nextjs,
+	typescript,
+	mui,
+	nodejs,
+	express,
+	mongodb,
+	graphql,
+	mongoose,
+	git,
+	github,
+	vim,
+	devtools,
+	prettier,
+	eslint,
+} from 'data/skills/AllSkills'
+
 import readImagePaths from 'utils/readImagePaths'
 
 export const CONFESSION = 'confession'
@@ -12,6 +32,12 @@ const description = [
 	`A social media application to help people overcome their imposter syndrome. It's name is Confession. It has user authentication, Newsfeed, picture upload and almost every feature that a social media application needs.`,
 	`The basic idea here is, if people could share their failure stories freely then other will not have the feeling of 'not good enough'. That's how we can kill our imposter syndrome.`,
 ]
+
+const frontEndTechs = [javascript, react, redux, nextjs, typescript, mui]
+
+const backEndTechs = [nodejs, express, typescript, mongodb, graphql, mongoose]
+
+const toolsUsed = [git, github, vim, devtools, prettier, eslint]
 
 const features: string[] = [
 	'User Authentication',
@@ -51,5 +77,8 @@ const confession = new Project(CONFESSION)
 	.addProjectInfos(projectInfos)
 	.addFeatures(features)
 	.addImages(readImagePaths(CONFESSION))
+	.addFrontEndTechs(frontEndTechs)
+	.addBackEndTechs(backEndTechs)
+	.addToolsUsed(toolsUsed)
 
 export default confession
