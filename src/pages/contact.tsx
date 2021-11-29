019b50c849e486ee2351ad53d1886fd71038a5c2
thatanjan <thatanjan@gmail.com>
2021-11-29 17:04:43 +0600

    some basic text added before the contact list.

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import { nanoid } from 'nanoid'
 
 import CenterLayout from 'components/Layouts/CenterLayout'
 import TitleHeader from 'components/Headers/TitleHeader'
@@ -16,6 +19,17 @@ const Contact = (props: Props) => {
 					description='I’M ALWAYS OPEN TO DISCUSSING PRODUCT DESIGN WORK OR PARTNERSHIPS.'
 				/>
 
+				<Box sx={{ mb: '5rem' }}>
+					{[
+						`I'm seeking out opportunities to collaborate with companies / agencies / individuals, not just work for them. I want to bring my collective design experience to the table where we can work together to solve real business-problems in a way that optimizes all of our respective experience and knowledge.`,
+
+						`I want to avoid subjective pissing-matches, and favor open-minded collaborators where egos are out of the equation. If that all sounds about right, then lets for sure chat about how we can work together.`,
+					].map((paragraph) => (
+						<Typography sx={{ mb: '1rem', fontSize: '1.2rem' }} key={nanoid()}>
+							{paragraph}
+						</Typography>
+					))}
+				</Box>
 				<ContactShow />
 			</CenterLayout>
 		</>
